9015eb5bd0abd15b48a27e3c8479434e9639da14
xmflzpfkxhqtm <choe.junekyung@gmail.com>
2025-04-18 18:26:59 +0900

    좆된 느낌

--- src/app/ask/page.tsx
+++ src/app/ask/page.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import { useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 import Image from 'next/image';
 import BottomNav from '../../../components/BottomNav';
