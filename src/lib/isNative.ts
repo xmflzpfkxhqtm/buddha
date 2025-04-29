1b06b7c18162cf22781e827576ae6074eacadbd6
xmflzpfkxhqtm <choe.junekyung@gmail.com>
2025-04-29 15:43:31 +0900

    4

--- src/lib/isNative.ts
+++ src/lib/isNative.ts
@@ -1,7 +1,8 @@
 export const isNative = () => {
     if (typeof window === 'undefined') return false;
   
-    // @ts-ignore
+    // ✅ 설명 붙인 ts-expect-error로 수정
+    // @ts-expect-error: Capacitor is only available in native app runtime
     return !!window.Capacitor;
   };
   
