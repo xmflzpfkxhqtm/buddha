5b688400c41a3c588ddc33fcc55410c88b3163b2
xmflzpfkxhqtm <choe.junekyung@gmail.com>
2025-05-04 23:26:39 +0900

    234

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -281,7 +281,7 @@ export default function Home() {
 </div>
           </div>
 
-          <div className="w-full h-[0.5px] opacity-50 bg-[#E0DBCF] mt-6" />
+          <div className="w-full h-[1px] opacity-30 bg-[#E0DBCF] mt-6" />
 
           <p className="text-sm font-medium text-white text-center mt-6 mb-24">
             &ldquo;연등&rdquo;은 누구나 수행하고 위로받을 수 있는 작은 법당입니다.
