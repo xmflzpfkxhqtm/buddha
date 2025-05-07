ead3152285ccfe7d57c20301d12934470c6cf2d4
xmflzpfkxhqtm <choe.junekyung@gmail.com>
2025-05-07 19:52:35 +0900

    fix: remove unnecessary ts-expect-error comment from isNative.ts

--- src/lib/isNative.ts
+++ src/lib/isNative.ts
@@ -3,8 +3,6 @@
 export const isNative = () => {
     if (typeof window === 'undefined') return false;
   
-    // ✅ 설명 붙인 ts-expect-error로 수정
-    // @ts-expect-error: Capacitor is only available in native app runtime
     return !!window.Capacitor;
   };
   
