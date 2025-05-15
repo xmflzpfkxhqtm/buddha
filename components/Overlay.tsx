ac8c17db74cb1c126b3d0c581728e114cec1ef17
xmflzpfkxhqtm <choe.junekyung@gmail.com>
2025-05-15 14:54:16 +0900

    5

--- components/Overlay.tsx
+++ components/Overlay.tsx
@@ -3,7 +3,7 @@
 export default function MarbleOverlay() {
   return (
     <div className="fixed inset-0 z-50 pointer-events-none">
-      <div className="w-full h-full bg-[url('/marble.png')] bg-repeat opacity-40 mix-blend-multiply" />
+      <div className="w-full h-full bg-[url('/marble.png')] bg-repeat opacity-10 mix-blend-multiply" />
     </div>
   );
 }
